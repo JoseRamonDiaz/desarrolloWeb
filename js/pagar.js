7bef313d081fa8f8c3d5d56b1be098b58ca05353
JoseRamonDiaz <slayermx_117@hotmail.com>
2014-05-26 14:26:17 -0500

    Vector con ids y cantidad de productos agregados en input hidden

--- js/pagar.js
+++ js/pagar.js
@@ -3,6 +3,29 @@ function muestraTotal(){
     label_total.innerHTML = "Total: "+calculaTotal();   
 }
 
+function crearMatrizProdCant(){
+    var numProductos = GetCookie("productos", document.cookie);
+    var matrizProdCant = new Array();
+    for(var i = 1; i <= numProductos; i++){
+        var vectorProdCant = new Array();
+        vectorProdCant.push(Number(GetCookie("prod_id"+i,document.cookie)));
+        vectorProdCant.push(Number(GetCookie("prod_cantidad"+i, document.cookie)));
+        matrizProdCant.push(vectorProdCant);
+    }
+    return JSON.stringify(matrizProdCant);
+}
+
+function crearInputHidden(){
+    var inputHidden = document.getElementById("prodCant");   
+    inputHidden.setAttribute("value",crearMatrizProdCant());
+}
+
+function verificarProdCant(){
+    console.log(document.getElementById("prodCant").value);
+}
+
 window.onload = function(){
     muestraTotal();
+    crearInputHidden();
+    verificarProdCant();
 }
